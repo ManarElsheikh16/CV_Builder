81de14c222d39d9e3d710b3b9b7f25fd3bf20d4c
ManarElsheikh16 <manar.abdalhafez@compit.aun.edu.eg>
2023-04-13 23:18:11 +0200

    Unit Testing

---
+++ Testing/Test/CommentTest.cs
@@ -0,0 +1,79 @@
+﻿using Final.Models;
+using Final.Repository;
+using NUnit.Framework;
+using Final;
+namespace Test
+{
+    [TestFixture]
+
+    public class CommentTest
+    {
+
+        Context context = new Context();
+        CommentRepository comment;
+      
+     
+        [Test]
+        public void CheckGetAll()
+        {
+            comment = new CommentRepository(context);
+            var result = comment.GetAll();
+            Assert.That(result.Count == GetCommentL());
+        }
+        [Test]
+        public void CheckGetCommentById()
+        {
+
+            comment = new CommentRepository(context);
+            var result = comment.GetById(1);
+            Assert.That(result.Id, Is.EqualTo(context.Comments.First().Id));
+        }
+
+        [Test]
+        public void ChcekCommentAdd()
+        {
+            comment = new CommentRepository(context);
+            var lastLength = context.Comments.ToList().Count;
+            comment.create();
+            Console.WriteLine(lastLength + GetCommentL());
+            Assert.That(lastLength, Is.LessThan(GetCommentL()));
+        }
+        [Test]
+        public void CommentIsDelete()
+        {
+
+            comment = new CommentRepository(context);
+            var lastLength = GetCommentL();
+            comment.Delete(6);
+            Assert.That(lastLength, Is.GreaterThan(GetCommentL()));
+
+        }
+        [Test]
+        public void CheckUpdate()
+        {
+            comment = new CommentRepository(context);
+            Comment NewComment = new Comment
+            {
+                Text = "nono bad",
+                isDeleted = false,
+                username = "Nada",
+                Date = new DateTime(),
+                TemplateId = 1,
+                image = "client-1.png",
+
+            };
+            comment.Update(1,NewComment );
+            Assert.That(comment.GetById(1).Text, Is.EqualTo("nono bad"));
+      
+        }
+
+
+        public int GetCommentL()
+        {
+            return context.Comments.Count();
+        }
+
+    }
+}
+
+
---
+++ Testing/Test/TemplateTest.cs
@@ -0,0 +1,86 @@
+﻿using Final.Models;
+using Final.Repository;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    [TestFixture]
+    public class TemplateTest
+    {
+
+
+        Context context = new Context();
+
+        TemplatesRepository template;
+        [Test]
+        public void CheckGetTempById()
+        {
+            template = new TemplatesRepository(context);
+            var result = template.GetById(1);
+            Assert.That(result.Id, Is.EqualTo(context.CvTemplates.First().Id));
+        }
+        [Test]
+        public void CheckGetAll()
+        {
+            template = new TemplatesRepository(context);
+            var result = template.GetAll();
+            Assert.That(result.Count == GetTemplateL());
+        }
+        [Test]
+        public void CheckGetCommentById()
+        {
+
+            template = new TemplatesRepository(context);
+            var result = template.GetById(1);
+            Assert.That(result.Id, Is.EqualTo(context.Comments.First().Id));
+        }
+
+        [Test]
+        public void ChcekCommentAdd()
+        {
+            template = new TemplatesRepository(context);
+            var lastLength = GetTemplateL();
+            template.create();
+          Assert.That(lastLength, Is.LessThan(GetTemplateL()));
+        }
+        [Test]
+        public void CommentIsDelete()
+        {
+
+            template = new TemplatesRepository(context);
+            var lastLength = GetTemplateL();
+            template.Delete(5);
+            Assert.That(lastLength, Is.GreaterThan(GetTemplateL()));
+
+        }
+        [Test]
+        public void CheckUpdate()
+        {
+            template = new TemplatesRepository(context);
+            CvTemplate NewTemplate = new CvTemplate
+            {
+                Img = "anthony.jpg",
+                comments = null,
+                Likes = 24,
+                isDeleted = false
+
+            };
+            template.Update(1, NewTemplate);
+            Assert.That(template.GetById(1).Likes, Is.EqualTo(24));
+
+        }
+
+
+        public int GetTemplateL()
+        {
+            return context.CvTemplates.Count();
+        }
+
+
+    }
+}
